e639eb55ae6e4b33409d5154278d5e42d8ed9125
haywah27 <hayleywahlroos15@gmail.com>
2020-11-29 18:12:20 -0800

    adding functionality to answer buttons

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ var mainEl = document.getElementById("main");
 var countdownSecondsLeft = 1;
 var gameTime = 60;
 
-i = 0;
+// var i = 0;
 var score = 0;
 var currentindex = 0;
-var questionEl= document.getElementById("question");
-// var answerEl = document.getElementById("question-answers");
+var questionEl = document.getElementById("question");
+var answerEl = document.getElementById("answers");
+
 var answerOne = document.getElementById("answerOne");
 var answerTwo = document.getElementById("answerTwo");
 var answerThree = document.getElementById("answerThree");
@@ -20,31 +21,84 @@ var answerFour = document.getElementById("answerFour");
 
 var messageDiv = document.querySelector("#message");
 
+var questionNumber = -1;
+var answer;
+
+
 
 var questions = [
 {
     title: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<javascript", "<script>", "<js", "<scripting>"],
+    choices: ["<javascript>", "<script>", "<js>", "<scripting>"],
     answer: "<script>"
 },
 {
     title: "How to write an IF statement in JavaScript?",
     choices: ["if i = 5", "if i = 5 then", "if (i === 5)", "if i === 5 then"],
     answer: "if (i === 5)"
+},
+{
+    title: "How does a FOR loop start?",
+    choices: ["for (i = 0; i <= 5)", "for (i <= 5; i++)", "for i = 1 to 5", "for (i = 0; i <= 5; i++)"],
+    answer: "for (i = 0; i <= 5; i++)"
+},
+{
+    title: "What is the correct way to write a JavaScript array?",
+    choices: ["var colors = (1:'red', 2:'green', 3:'blue')", "var colors = ['red', 'green', 'blue']", "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", "var colors = 'red', 'green', 'blue'"],
+    answer: "var colors = ['red', 'green', 'blue']"
+},
+
+{
+    title: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["onclick", "onmouseclick", "onchange", "onmouseover"],
+    answer: "onclick"
 }
 ]
 
 
-function displayQuestion() {
-    for(var i = 0; i < questions.length; i++){
-    answerOne.innerText = questions[i].choices[0];
-    answerTwo.innerText = questions[i].choices[1];
-    answerThree.innerText = questions[i].choices[2];
-    answerFour.innerText = questions[i].choices[3];
-    }
 
- 
+
+
+
+function displayQuestion() {
+        questionNumber++;
+        if (questionNumber < questions.length){
+            answer = questions[questionNumber].answer
+            questionEl.textContent = questions[questionNumber].title;
+            answerEl.innerHTML = "";
+        
+            var choices = questions[questionNumber].choices;
+        
+            for (var q = 0; q < choices.length; q++) {
+                var nextChoice = document.createElement("button");
+        
+                nextChoice.textContent = choices[q]
+                
+                // answerBtn = answerEl.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+                answerBtn = answerEl.appendChild(nextChoice);
+            }
+
+        } else {
+            endGame();
+        }
 }
+
+answerEl.addEventListener("click", function (event) {
+    var pEl= document.getElementsByClassName("feedback")[0]
+    
+    // evaluation of user's answer choices & feedback
+    if (answer === event.target.textContent) {   
+        pEl.innerHTML = "Correct!";
+        // setTimeout(hideFeedback,1000);
+        // showFeedback();   
+    } else {
+        pEl.innerHTML = "Sorry, that's incorrect.";
+        // setTimeout(hideFeedback,1000);
+        // secondsLeft = secondsLeft - 10;
+        // showFeedback();
+    }    
+    displayQuestion();
+});
 
 
 
