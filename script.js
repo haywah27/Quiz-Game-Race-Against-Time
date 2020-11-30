bfdfc7ff63bc4ed72fee10aaab3976e1766ebe6b
haywah27 <hayleywahlroos15@gmail.com>
2020-11-29 16:57:28 -0800

    adding answer button attributes

--- script.js
+++ script.js
@@ -5,8 +5,48 @@ var gameTimeClock = document.getElementById("gameTimeClock")
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 
-var countdownSecondsLeft = 4;
+var countdownSecondsLeft = 1;
 var gameTime = 60;
+
+i = 0;
+var score = 0;
+var currentindex = 0;
+var questionEl= document.getElementById("question");
+// var answerEl = document.getElementById("question-answers");
+var answerOne = document.getElementById("answerOne");
+var answerTwo = document.getElementById("answerTwo");
+var answerThree = document.getElementById("answerThree");
+var answerFour = document.getElementById("answerFour");
+
+var messageDiv = document.querySelector("#message");
+
+
+var questions = [
+{
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<javascript", "<script>", "<js", "<scripting>"],
+    answer: "<script>"
+},
+{
+    title: "How to write an IF statement in JavaScript?",
+    choices: ["if i = 5", "if i = 5 then", "if (i === 5)", "if i === 5 then"],
+    answer: "if (i === 5)"
+}
+]
+
+
+function displayQuestion() {
+    for(var i = 0; i < questions.length; i++){
+    answerOne.innerText = questions[i].choices[0];
+    answerTwo.innerText = questions[i].choices[1];
+    answerThree.innerText = questions[i].choices[2];
+    answerFour.innerText = questions[i].choices[3];
+    }
+
+ 
+}
+
+
 
 // start button to start quiz
 startButton.addEventListener("click", function() {
@@ -25,11 +65,13 @@ function countDown() {
       clearInterval(countDownInterval);
     //   countDownInterval.remove();
       gameClock();
+      displayQuestion();
     }
 
   }, 1000);
 }
 
+// game time countdown
 function gameClock(){
     timeEl.textContent = " ";
     var timerInterval = setInterval(function() {
@@ -44,6 +86,8 @@ function gameClock(){
     }, 100);
 }
 
+
+
 function endGame() {
     gameTimeClock.textContent = " ";
 
@@ -56,4 +100,3 @@ function endGame() {
 
 }
 
-// setTime();
