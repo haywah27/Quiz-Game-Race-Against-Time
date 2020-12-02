53b9be5b3671471e629c84dc21c2a0640c5bcf2a
haywah27 <hayleywahlroos15@gmail.com>
2020-12-01 17:37:09 -0800

    commenting and added navbar

--- scoreBoard.js
+++ scoreBoard.js
@@ -1,31 +1,36 @@
-var restartBtn = document.querySelector("button.restartBtn"),
-    clearBtn = document.querySelector("button.clearBtn"),
-    // get the highScores list and turn it back into an object
-    highScores = JSON.parse(localStorage.getItem("highScores") || "[]"),
-    scoreList = document.getElementById("score-list");
+// querySelectors for buttons on scoreBoard page
+var restartBtn = document.querySelector(".restartBtn");
+var clearBtn = document.querySelector(".clearBtn");
 
-    // sort scores from high to low
-    highScores.sort(function (a,b){
-        return b.score - a.score
-    })
+// get the highScores list and turn it back into an object
+var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
 
-    // display the scores
-    for (var i = 0; i < highScores.length; i += 2) {
-        var newLi = document.createElement("li")
-        console.log(newLi)
-        newLi.textContent = highScores[i].name + " - " + highScores[i].score
-        scoreList.appendChild(newLi)
-    }
+// querySelector for list on scoreBoard page
+var scoreList = document.getElementById("score-list");
 
 
-// click handlers for restart and clearing scoreboard
+// sort scores from high to low
+highScores.sort(function (a, b){
+    return b.score - a.score
+})
+
+// display the scores by appending each new item
+for (var i = 0; i < highScores.length; i += 2) {
+    var newList = document.createElement("li")
+    console.log(newList)
+    newList.textContent = highScores[i].name + " - " + highScores[i].score
+    scoreList.appendChild(newList)
+}
+
+// click handler for returning to start page
+restartBtn.addEventListener("click", function () {
+    window.location.href = './index.html'
+});
+
+// click handler for clearing scoreboard
 clearBtn.addEventListener("click", function () {
     localStorage.clear();
     scoreList.innerHTML = " ";
-    // history.back()
 });
-restartBtn.addEventListener("click", function () {
-    window.location.href = './index.html'
-   
-});
+
 
--- script.js
+++ script.js
@@ -1,42 +1,40 @@
+// main divs on html
+var startPage = document.getElementById("start-page");
+var quizEl = document.getElementById("quiz");
+
 // start button
 var startButton = document.getElementById("startButton");
 
 // game time clock element
 var gameTimeClock = document.getElementById("gameTimeClock");
 
-// score throughout game element
-var scoreTally = document.getElementById("scoreTally");
-
 // time element for interval countdown
 var timeEl = document.querySelector(".time");
-
-// 3 main divs on html
-var startPage = document.getElementById("start-page");
-var quizEl = document.getElementById("quiz");
-var scoreForm = document.querySelector("#submit-score");
 
 // game countdown clock
 var gameTime = 61;
 
 // inital score value
 var score = 0;
+
+// score throughout game element
+var scoreTally = document.getElementById("scoreTally");
+
 // questions
 var questionEl = document.getElementById("question");
+
 // answers
 var answerEl = document.getElementById("answers");
 
 // inital counter of questions position
 var questionNumber = -1;
-var answer;
 
-// final score display element
+// final score display elements
 var finalScore = document.getElementById("finalScore");
-var userScore = document.getElementById("user-score");
 
-var highScores = [];
-
-var scoreForm = document.querySelector("#submit-score");
+// submitting score elements
 var submitBtn = document.querySelector("button.submitBtn")
+var scoreForm = document.querySelector("#score-list");
 
 // list of questions in object
 var questions = [
@@ -68,22 +66,20 @@ var questions = [
 }
 ]
 
-// page setup
+// hiding bullet points on start page
 answerEl.textContent = "";
-// hide score submission box
+
+// hide score submission box on start page 
 document.getElementById("submit-score").style.display = "none";
 
 
-// start button to start quiz
+// start button event listener to start quiz
 startButton.addEventListener("click", function() {
-    // startPage.textContent = " ";
-    
     document.getElementById("start-page").classList.add('d-none');
     document.getElementById("quiz").classList.remove('d-none');
     gameClock();
     displayQuestion();    
 });
-
 
 
 // game time countdown
@@ -100,12 +96,13 @@ function gameClock(){
     }, 1000);
 }
 
-
-
+// display questions, one at a time, with correlating answer choices
 function displayQuestion() {
-    
+    // display user score 
     scoreTally.textContent = ("Score: " + score);
+    // go to the first question
     questionNumber++;
+    // if the question is less than the amount of questions, display question and choices
     if (questionNumber < questions.length){
         answer = questions[questionNumber].answer
         questionEl.textContent = questions[questionNumber].title;
@@ -117,33 +114,37 @@ function displayQuestion() {
             var nextChoice = document.createElement("button");
     
             nextChoice.textContent = choices[q]
-            
-            // answerBtn = answerEl.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+            // 
             answerBtn = answerEl.appendChild(nextChoice);
-        }
+                        // answerBtn = answerEl.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
 
+        }
+    // if questions run out, initiate end of game
     } else {
         endGame()
     }
 }
 
 
-
+// button click response
 answerEl.addEventListener("click", function (event) {
     var pEl= document.getElementsByClassName("feedback")[0]
 
-    // evaluation of user's answer choices & feedback
+    // evaluation of user's answer choices 
+    // if correct, add to score, display positive feedback
     if (answer === event.target.textContent) {   
         score++;
         pEl.innerHTML = "Correct!";
-        setTimeout(hideFeedback,1500);
+        setTimeout(hideFeedback, 1500);
         showFeedback();   
     } else {
+        //  if wrong, subtract from time, display negative feedback
         gameTime = gameTime - 10;
         pEl.innerHTML = "Sorry, that's incorrect.";
-        setTimeout(hideFeedback,1500);
+        setTimeout(hideFeedback, 1500);
         showFeedback();
-    }    
+    } 
+    // run displayQuestion function   
     displayQuestion();
 });
 
@@ -161,8 +162,9 @@ function showFeedback(){
 }
 
 
-
+// end game when timer or out of questions function
 function endGame() {
+    // hide all content
     gameTimeClock.textContent = " ";
     answerEl.textContent = " ";
     questionEl.textContent = " ";
@@ -173,35 +175,33 @@ function endGame() {
     document.getElementById("submit-score").style.display = "";
     finalScore.textContent = ("Your final score is: " + score + "!");
 
+    // button changes screen and appends new score to list
     submitBtn.addEventListener("click", function (event) {
-        // event.stopPropagation();
-        addScore();
-        
+        appendScore();
+        // relocate to top score page
         window.location.href = './topScores.html'
     });
-
 }
 
-var scoreForm = document.querySelector("#score-list");
 
-function addScore () {
+// appending new scores function
+function appendScore() {
+    // user name input element
     userNameInput = document.getElementById("userName").value
     
     // create a new object with name and score keys
     var newScore = {
             name: userNameInput,
             score: score
-        };
+    };
         
         // check if there are scores in local storage first(get it)
-            //if not, make a new/blank array
-            var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
-            // push object into score array
-            highScores.push(newScore)
-            // turn objects into an array of strings then put it into local storage
-            localStorage.setItem("highScores", JSON.stringify(highScores));
-
-
+         //if not, make a new/blank array
+         var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+         // push object into score array
+         highScores.push(newScore)
+         // turn objects into an array of strings then put it into local storage
+         localStorage.setItem("highScores", JSON.stringify(highScores));
 }
 
 
